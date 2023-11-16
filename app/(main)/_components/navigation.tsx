7b067c38d0cd5bd1aee2f0ea66d36045f29e7c0c
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-11-16 22:19:11 +0530

    settings

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -25,11 +25,14 @@ import {
   PopoverContent,
 } from "@/components/ui/popover";
 import {TrashBox} from "./trash-box"
+import { useSearch } from "@/hooks/use-search";
+import { useSettings } from "@/hooks/use-settings";
 
 
 
 export const Navigation = () => {
-  
+  const settings = useSettings();
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -150,12 +153,12 @@ export const Navigation = () => {
             label="Search"
             icon={Search}
             isSearch
-            onClick={()=>{}}
+            onClick={search.onOpen}
           />
           <Item
             label="Settings"
             icon={Settings}
-            onClick={()=>{}}
+            onClick={settings.onOpen}
           />
           <Item 
           onClick={handleCreate}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
 import { Toaster } from 'sonner'
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         disableTransitionOnChange
         storageKey='visionarium-theme-2'>
           <Toaster position='bottom-center' />
+          <ModalProvider /> 
           {children}
         </ThemeProvider>
         </ConvexClientProvider>

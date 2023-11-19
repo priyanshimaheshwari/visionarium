bec6e4ba61b2cb43c4ca76e23d6122464f7ea2f9
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-11-19 22:18:28 +0530

    image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
 import { Toaster } from 'sonner'
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { EdgeStoreProvider } from '@/lib/edgestore'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
+          <EdgeStoreProvider>
         <ThemeProvider 
         attribute='class'
         defaultTheme='system'
@@ -32,6 +34,7 @@ export default function RootLayout({
           <ModalProvider /> 
           {children}
         </ThemeProvider>
+        </EdgeStoreProvider>
         </ConvexClientProvider>
        </body>
     </html>
--- convex/documents.ts
+++ convex/documents.ts
@@ -1,3 +1,5 @@
+"use client"
+
 import { v } from "convex/values";
 
 import { mutation, query } from "./_generated/server";
@@ -331,6 +333,35 @@ export const removeIcon = mutation({
 
     const document = await ctx.db.patch(args.id, {
       icon: undefined
+    });
+
+    return document;
+  }
+});
+
+export const removeCoverImage = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      coverImage: undefined,
     });
 
     return document;
